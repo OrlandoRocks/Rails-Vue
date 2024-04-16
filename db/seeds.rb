04da180bf167ce247240b97f00640948695e889e
LandoApex <odelapena@apexsystems.com>
2024-04-16 17:57:25 -0600

    db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+admin_role = Role.create(name: 'admin')
+client_role = Role.create(name: 'client')
+user_role = Role.create(name: 'user')
+
+User.create(email: 'admin@admin.com', password: 'qwe123', name:'Admin', last_name: 'Admin', role: admin_role)
+User.create(email: 'client@client.com', password: 'qwe123', name:'Client', last_name: 'Client', role: client_role)
